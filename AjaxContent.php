e191d3546755f0013e98cf4e58533f7aad98e171
Alexshleg <alex.shleg@yandex.ru>
2015-01-30 15:16:34 +0300

    ajaxUpdate

---
+++ AjaxContent.php
@@ -0,0 +1,22 @@
+<?php
+
+header("Content-Type: text/html; charset=UTF-8");
+$count = $_POST['count'];
+$mysqli = new Mysqli('localhost', 'root', '', 'malmart');
+$mysqli->query("SET NAMES utf8");
+$r = array();
+$result = $mysqli->query("SELECT * FROM `tbl_chat`");
+while($row = $result->fetch_assoc()) {
+    $r[] = $row;
+}
+if(empty($r)) {
+    echo "empty";
+} else {
+    for ($i=0;$i < count($r);$i++) {
+        $data=$r[$i];
+        $rows[$i] = '['.date('d.m.Y  H:i:s',$data['created']).'] '.$data['username'].': '.$data['message'];
+        echo '<tr><td>'.$rows[$i].'</td></tr>';
+    }
+    //echo json_encode($r);
+}
+?>
